e5e0a9c5fbfd7b6e522def6138cc71a53b4c333b
Aytaditya <adityaaryan531@gmail.com>
2024-05-26 18:32:38 +0530

    Apollo server

--- apollo-server/index.js
+++ apollo-server/index.js
@@ -1,37 +1,47 @@
 import { ApolloServer } from "@apollo/server";
-import {startStandaloneServer} from "@apollo/server/standalone";
-import {typeDefs} from "./schema.js";
-
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "./schema.js";
 import db from "./db.js";
 
-const resolvers={
-    Query:{
-        games(){
-            return db.games
-        },
-        reviews(){
-            return db.reviews
-        },
-        authors(){
-            return db.authors
+const resolvers = {
+  Query: {
+    games() {
+      return db.games;
+    },
+    reviews() {
+      return db.reviews;
+    },
+    authors() {
+      return db.authors;
+    },
+    review(_, args) {
+      return db.reviews.find((review) => review.id === args.id);
+    },
+    game(_,args){
+        return db.games.find((game)=>game.id === args.id);
+    },
+
+  },
+    Game:{
+        reviews(parent){
+            return db.reviews.filter((review)=>review.game_id === parent.id);
         }
     }
-}
-
+};
 
 // server setup
-const server=new ApolloServer({
-    typeDefs,
-    resolvers,
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
-})
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
 
+const port = 4000;
+console.log(`Server ready at http://localhost:${port}`);
 
-const {url}=await startStandaloneServer(server,{
-    listen:{port:4000}
-})
-
-console.log(`Server ready at, `,4000)
 
 
 //resolvers handle any incoming requests and return the appropriate data
--- apollo-server/schema.js
+++ apollo-server/schema.js
@@ -1,32 +1,31 @@
-export const typeDefs=`#graphql
-type Game{
-    id:ID!,
-    title:String!,
-    platform:[String!]!,
-
-}
-type Review{
-    id:ID!,
-    rating:Int!,
-    content:String!,
-}
-type Author{
-    id:ID!,
-    name:String!,
-    verified:Boolean!,
-}
-type Query{
-    games:[Game],
-    reviews:[Review],
-    authors:[Author]
+export const typeDefs = `#graphql
+type Game {
+    id: ID!
+    title: String!
+    platform: [String!]!
+    reviews: [Review!]
 }
 
+type Review {
+    id: ID!
+    rating: Int!
+    content: String!
+    game:Game!
+    author:Author!
+}
+
+type Author {
+    id: ID!
+    name: String!
+    verified: Boolean!
+    reviews: [Review!]
+}
+
+type Query {
+    games: [Game]
+    game(id:ID!): Game
+    reviews: [Review]
+    review(id: ID!): Review
+    authors: [Author]
+}
 `
-
-
-// id:ID, id is type in graphql
-// platform:[String], array of strings
-// adding exclamation mark to a type makes it required
-
-
-//type query is used to define entry points to the graph and every schema should have it 
